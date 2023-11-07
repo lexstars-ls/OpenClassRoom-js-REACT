1d90892899ae6891a07b67d06647fb8d1a7f50ba
Sisovath lucas <lucas.sisovath@egs.school>
2023-11-07 11:56:26 +0100

    rename file

---
+++ reactprojet1/src/Footer.js
@@ -0,0 +1,10 @@
+function Footer () {
+    return (
+        <footer>
+            <p>Corps céleste gravitant autour d'une planète.
+             La Lune est le satellite de la Terre.</p>
+        </footer>
+    )
+}
+
+export default Footer;
